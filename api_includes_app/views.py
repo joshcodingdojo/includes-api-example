ca69e9758d2fa594cac439090983c591f6951fde
joshcodingdojo <jgendal@codingdojo.com>
2021-06-02 22:12:05 -0400

    added poke api example

--- api_includes_app/views.py
+++ api_includes_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import requests
 poke_api_url = "https://pokeapi.co/api/v2/pokemon"
 # Create your views here.
 def index(request): 
-  return render(request, 'index.html')
+  r = requests.get(poke_api_url)
+  json = r.json()
+  print(json['results'])
+  context = {
+    "results": json['results']
+  }
+  return render(request, 'index.html', context)
