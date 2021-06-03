76875d5b2ea8af2581d11896e2bf11e0eefbcbec
joshcodingdojo <jgendal@codingdojo.com>
2021-06-02 21:53:24 -0400

    initial

---
+++ api_includes_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+poke_api_url = "https://pokeapi.co/api/v2/pokemon"
+# Create your views here.
+def index(request): 
+  return render(request, 'index.html', context)
