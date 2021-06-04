3e0b4924b537600255b045ce0408d964bbab7f34
joshcodingdojo <jgendal@codingdojo.com>
2021-06-03 22:12:38 -0400

    added contact functionality

--- api_includes_app/models.py
+++ api_includes_app/models.py
@@ -1,3 +1,4 @@
 from django.db import models
+from django.db.models.fields.related import OneToOneField
 
 # Create your models here.
--- api_includes_app/views.py
+++ api_includes_app/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 import requests
 poke_api_url = "https://pokeapi.co/api/v2/pokemon"
 # Create your views here.
-def index(request): 
+def index(request):
   r = requests.get(poke_api_url)
-  json = r.json()
-  print(json['results'])
+  r = r.json()
+  results = r['results']
   context = {
-    "results": json['results']
+    "pokemon": results
   }
   return render(request, 'index.html', context)
+
+def contact(request):
+  return render(request, 'contact.html')
